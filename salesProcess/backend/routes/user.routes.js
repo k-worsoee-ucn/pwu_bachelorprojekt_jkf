d61cdf94589212c5a52774881ded906751d1ba24
NikolajHolmgren <nikolaj.holmgren@hotmail.com>
2025-12-02 11:02:05 +0100

    Removed admin functionality completely

--- salesProcess/backend/routes/user.routes.js
+++ salesProcess/backend/routes/user.routes.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
-const { verifyToken, requireRole } = require("../middleware/auth");
+const { verifyToken } = require("../middleware/auth");
 const {
   validateUserLogin,
   validateUserCreation,
   validateId
 } = require("../middleware/validation");
-
-const requireAdmin = requireRole(['admin']);
 
 // Public routes
 router.post("/login", validateUserLogin, userController.loginUser);
@@ -17,9 +15,9 @@ router.post("/login", validateUserLogin, userController.loginUser);
 router.get("/me", verifyToken, userController.getCurrentUser);
 router.get("/:id", verifyToken, validateId, userController.getUserById);
 router.get("/", verifyToken, userController.getAllUsers);
-router.post("/", verifyToken, requireAdmin, validateUserCreation, userController.createUser);
+router.post("/", verifyToken, validateUserCreation, userController.createUser);
 router.put("/:id", verifyToken, validateId, userController.updateUser);
-router.delete("/:id", verifyToken, requireAdmin, validateId, userController.deleteUser);
+router.delete("/:id", verifyToken, validateId, userController.deleteUser);
 
 router.get("/:id/processes", verifyToken, validateId, userController.getUserProcesses);
 router.get("/:id/customers", verifyToken, validateId, userController.getUserCustomers);
---
+++ salesProcess/frontend/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
+      <RouterLink to="/my-processes">My Processes</RouterLink>
+      <RouterLink to="/all-processes">All Processes</RouterLink>
+      <RouterLink to="/create-new-process">Create New Process</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  nav {
+    padding: 1rem;
+    border-bottom: 1px solid #ccc;
+
+    a {
+      margin-right: 1rem;
+      text-decoration: none;
+      color: #333;
+
+      &:hover {
+        color: #666;
+      }
+    }
+  }
+}
+</style>
